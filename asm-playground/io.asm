c0b73e716ed1d8f09bead31a07ef3b894b215ac5
TobTobXX <filiuspatrisapps@gmail.com>
2021-08-17 11:10:24 +0200

    End-of-lesson commit

---
+++ asm-playground/io.asm
@@ -0,0 +1,37 @@
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+; To assemble and run:
+;
+;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+; ----------------------------------------------------------------------------------------
+
+	global		_start
+
+	section		.text
+_start:
+	mov			rdi, 0x41
+	call		_output
+	leave
+	ret
+
+
+_output:
+	; push		rbp
+	; mov			rbp+0x1,rsp
+	enter		0x1
+	mov			rax, 1                  ; system call for write
+	mov			rdi, 1                  ; file handle 1 is stdout
+	mov			rsi, message            ; address of string to output
+	mov			rdx, 13                 ; number of bytes
+	syscall                             ; invoke operating system to do the write
+	mov			rax, 60                 ; system call for exit
+	xor			rdi, rdi                ; exit code 0
+	syscall                             ; invoke operating system to exit
+	leave
+	ret
+
+
+
+	section	.data
+message:
+	db		"Hello, World", 10          ; note the newline at the end
